9da3ebda2f7fbe892547df88a96e3f1d5d40911d
Bryan <bav29cr@gmail.com>
2023-07-05 18:08:06 -0400

    Responsive Title animations Bug fixed 1.1

--- app/app.js
+++ app/app.js
@@ -2,15 +2,24 @@
 gsap.registerPlugin(ScrollTrigger);
 
 
-// Responsive
 let mm = gsap.matchMedia();
 
-let mm1 = gsap.matchMedia();
 
-mm.add("(min-width: 1025px)", () => {
+let w = window.innerWidth/2;
+
+const aboutW = document.getElementById('about').offsetWidth/2;
+const whatW = document.getElementById('what').offsetWidth/2;
+const skillsW = document.getElementById('skills').offsetWidth/2;
+const projectsW = document.getElementById('projects').offsetWidth/2;
+
+console.log(aboutW);
+
+console.log(w);
+
+// mm.add("(min-width: 1025px)", () => {
   gsap.to("#about", {
-    x: '41vw',
-    xPercent: -100,
+     x:w-aboutW,
+     margin: 0,
     duration: 1,
     ease: "power2.inOut",
     scrollTrigger: {
@@ -23,8 +32,8 @@ mm.add("(min-width: 1025px)", () => {
   });
   
   gsap.to("#what", {
-      x: '-41vw',
-      xPercent: 100,
+    x:-(w-whatW),
+    margin: 0,
       duration: 1,
       ease: "power2.inOut",
       scrollTrigger: {
@@ -36,8 +45,8 @@ mm.add("(min-width: 1025px)", () => {
     });
   
     gsap.to("#skills", {
-      x: '41vw',
-      xPercent: -100,
+      x:w-skillsW,
+      margin: 0,
       duration: 1,
       ease: "power2.inOut",
       scrollTrigger: {
@@ -49,8 +58,8 @@ mm.add("(min-width: 1025px)", () => {
     });
   
     gsap.to("#projects", {
-      x: '-41vw',
-      xPercent: 100,
+      x:-(w-projectsW),
+      margin: 0,
       duration: 1,
       ease: "power2.inOut",
       scrollTrigger: {
@@ -62,171 +71,61 @@ mm.add("(min-width: 1025px)", () => {
     });
   
     
-});
+// });
 
-mm.add("(max-width: 1024px)", () => {
-  gsap.to("#about", {
-      x: '51vw',
-      xPercent: -100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#about",
-        start: "top 35%",
-        end: "top 35%",
-        markers: true,
-        scrub: 2
-      }
-    });
+// mm.add("(max-width: 1024px)", () => {
+//   gsap.to("#about", {
+//       x: '51vw',
+//       xPercent: -100,
+//       duration: 1,
+//       ease: "power2.inOut",
+//       scrollTrigger: {
+//         trigger: "#about",
+//         start: "top 35%",
+//         end: "top 35%",
+//         markers: true,
+//         scrub: 2
+//       }
+//     });
 
-    gsap.to("#what", {
-      x: '-51vw',
-      xPercent: 100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#what",
-        start: "top 35%",
-        end: "top 35%",
-        scrub: 2
-      }
-    });
+//     gsap.to("#what", {
+//       x: '-51vw',
+//       xPercent: 100,
+//       duration: 1,
+//       ease: "power2.inOut",
+//       scrollTrigger: {
+//         trigger: "#what",
+//         start: "top 35%",
+//         end: "top 35%",
+//         scrub: 2
+//       }
+//     });
 
-    gsap.to("#skills", {
-      x: '51vw',
-      xPercent: -100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#skills",
-        start: "top 35%",
-        end: "top 35%",
-        scrub: 2
-      }
-    });
+//     gsap.to("#skills", {
+//       x: '51vw',
+//       xPercent: -100,
+//       duration: 1,
+//       ease: "power2.inOut",
+//       scrollTrigger: {
+//         trigger: "#skills",
+//         start: "top 35%",
+//         end: "top 35%",
+//         scrub: 2
+//       }
+//     });
 
-    gsap.to("#projects", {
-      x: '-51vw',
-      xPercent: 100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#projects",
-        start: "top 35%",
-        end: "top 35%",
-        scrub: 2
-      }
-    });
+//     gsap.to("#projects", {
+//       x: '-51vw',
+//       xPercent: 100,
+//       duration: 1,
+//       ease: "power2.inOut",
+//       scrollTrigger: {
+//         trigger: "#projects",
+//         start: "top 35%",
+//         end: "top 35%",
+//         scrub: 2
+//       }
+//     });
     
-});
-mm1.add("(max-width: 576px)", () => {
-  gsap.to("#about", {
-      x: '60vw',
-      xPercent: -100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#about",
-        start: "top 25%",
-        end: "top 25%",
-        scrub: 2
-      }
-    });
+// });
 
-    gsap.to("#what", {
-      x: '-65vw',
-      xPercent: 100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#what",
-        start: "top 25%",
-        end: "top 25%",
-        scrub: 2
-      }
-    });
-
-    gsap.to("#skills", {
-      x: '60vw',
-      xPercent: -100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#skills",
-        start: "top 25%",
-        end: "top 25%",
-        markers: true,
-        scrub: 2
-      }
-    });
-
-    gsap.to("#projects", {
-      x: '-55vw',
-      xPercent: 100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#projects",
-        start: "top 25%",
-        end: "top 25%",
-        scrub: 2
-      }
-    });
-    
-});
-
-mm1.add("(max-width: 375px)", () => {
-  gsap.to("#about", {
-      x: '63vw',
-      xPercent: -100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#about",
-        start: "top 50%",
-        end: "top 50%",
-        markers: true,
-        scrub: 2
-      }
-    });
-
-    gsap.to("#what", {
-      x: '-71vw',
-      xPercent: 100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#what",
-        start: "top 50%",
-        end: "top 50%",
-        scrub: 2
-      }
-    });
-
-    gsap.to("#skills", {
-      x: '60vw',
-      xPercent: -100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#skills",
-        start: "top 50%",
-        end: "top 50%",
-        scrub: 2
-      }
-    });
-
-    gsap.to("#projects", {
-      x: '-65vw',
-      xPercent: 100,
-      duration: 1,
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: "#projects",
-        start: "top 50%",
-        end: "top 50%",
-        scrub: 2
-      }
-    });
-    
-});
